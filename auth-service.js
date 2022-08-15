5101e895ddfa29ed3167575676a03fd8d48078d7
Your Name <you@example.com>
2022-08-14 23:04:42 -0400

    first commit

--- auth-service.js
+++ auth-service.js
@@ -22,7 +22,7 @@ let User;
 
 module.exports.initialize = function () {
     return new Promise(function (resolve, reject) {
-    let db = mongoose.createConnection("mongodb+srv://dbUser:<Rudransh$888>@senecaweb.uoazufp.mongodb.net/?retryWrites=true&w=majority");
+    let db = mongoose.createConnection("mongodb+srv://dbUser:<Rudransh$888>@senecaweb.uoazufp.mongodb.net/web322_week8.");
     db.on('error', (err)=>{
     reject(err);
     });
